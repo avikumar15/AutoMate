5ea95fb9d220a03d34c2811fc7a2381018e3c42b
shreyshrey71 <shreyshrey71@github.com>
2020-02-08 08:17:42 +0000

    change zoom point for map activity

--- app/src/main/java/com/example/automate/LocationSelectionActivity.java
+++ app/src/main/java/com/example/automate/LocationSelectionActivity.java
@@ -30,14 +30,14 @@ public class LocationSelectionActivity extends AppCompatActivity {
                 "Beryl", "Aquamarine", "Amber", "Zircon", "Shopping Complex", "Guest House",
                 "Library", "Sports Center", "Main Gate", "Hospital", "Mega Mess 1",
                 "Mega Mess 2", "F Mess", "DoMS"));
-        latitude = new ArrayList<Float>(Arrays.asList(10.760995f, 10.759899f,
-                10.758079f, 10.764033f, 10.762894f, 10.763819f, 10.764254f, 10.764134f, 10.762963f,
-                10.761845f, 10.767950f, 10.767847f, 10.766150f, 10.760996f, 10.753957f,
+        latitude = new ArrayList<Float>(Arrays.asList(10.760575f, 10.759848f,
+                10.757727f, 10.763066f, 10.761669f, 10.763229f, 10.763133f, 10.764208f, 10.762964f,
+                10.761878f, 10.767950f, 10.767847f, 10.766150f, 10.760996f, 10.753957f,
                 10.758213f, 10.758150f, 10.757031f, 10.762377f, 10.765167f,
                 10.764594f, 10.764134f, 10.761326f));
-        longitude = new ArrayList<Float>(Arrays.asList(78.808740f, 78.810872f,
-                78.821780f, 78.814994f, 78.813449f, 78.817760f, 78.817576f, 78.815354f, 78.812004f,
-                78.817473f, 78.817672f, 78.813085f, 78.817133f, 78.818521f, 78.819815f,
+        longitude = new ArrayList<Float>(Arrays.asList(78.808391f, 78.811164f,
+                78.820660f, 78.814409f, 78.813316f, 78.817823f, 78.816337f, 78.815447f, 78.812003f,
+                78.817475f, 78.817672f, 78.813085f, 78.817133f, 78.818521f, 78.819815f,
                 78.818068f, 78.816590f, 78.813336f, 78.818472f, 78.815113f,
                 78.813110f, 78.815354f, 78.816446f));
         locationAdapter = new LocationAdapter(this, location, latitude, longitude);
